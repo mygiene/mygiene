ce8c0ec5a8dea552386754925c49deaeeb4e6138
Rakshit Maini <rakshit.maini@bivocalbirds.com>
2021-07-10 20:45:28 +0530

    local storage ui logic

--- components/kit/index.js
+++ components/kit/index.js
@@ -1,56 +1,61 @@
 import { useEffect, useState } from "react";
+import { IsCSR } from "../../util/common";
 import StyledWrapper from "./style.kit";
-const initialState = [
+const allProd = [
   { pId: "product1", pName: "kit 1" },
   { pId: "product2", pName: "kit 2" },
 ];
 
 export const Kit = () => {
-  const [products, setproducts] = useState(initialState);
+  // const cartList = JSON.parse(localStorage.getItem("cart"));
+  // console.log({ cartList });
+
+  const [cart, setcart] = useState([]);
+  const [products, setproducts] = useState(allProd);
 
   useEffect(() => {
-    // products.map((m) => {
-    //   const p = localStorage.getItem(m.pId);
-    //   setproducts([...products]);
-    // });
-    const items = { ...localStorage };
-    console.log(JSON.parse(JSON.stringify(items)));
+    const storedData = JSON.parse(localStorage.getItem("cart"));
+    if (storedData) {
+      setcart(storedData);
+    }
   }, []);
 
-  function add(pid) {
-    const oldProductList = localStorage.getItem(pid);
-    console.log({ oldProductList });
-    if (!oldProductList)
-      localStorage.setItem(pid, JSON.stringify({ pid, qt: 1 }));
-    else {
-      const parsedOldList = JSON.parse(oldProductList);
-      localStorage.setItem(
-        pid,
-        JSON.stringify({ pid, qt: parsedOldList.qt + 1 })
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  function add(product) {
+    const prod = cart.find((p) => p.pId === product.pId);
+    if (!prod) {
+      setcart((oldCart) => [...oldCart, { ...product, qt: 1 }]);
+    } else {
+      const updatedCart = cart.map((p) =>
+        p.pId === prod.pId ? { ...p, qt: p.qt + 1 } : { ...p }
       );
+      setcart(updatedCart);
     }
   }
-  function remove(pid) {
-    const oldProductList = localStorage.getItem(pid);
-    console.log({ oldProductList });
-    if (!oldProductList)
-      localStorage.setItem(pid, JSON.stringify({ pid, qt: 0 }));
-    else {
-      const parsedOldList = JSON.parse(oldProductList);
-      if (parsedOldList.qt > 0)
-        localStorage.setItem(
-          pid,
-          JSON.stringify({ pid, qt: parsedOldList.qt - 1 })
-        );
+  function remove(product) {
+    const prod = cart.find((p) => p.pId === product.pId);
+    if (!prod) {
+      //skip and do nothing
+    } else if (prod.qt === 1) {
+      const updatedCart = cart.filter((f) => f.pId !== prod.pId);
+      setcart(updatedCart);
+    } else {
+      const updatedCart = cart.map((p) =>
+        p.pId === prod.pId ? { ...p, qt: (p.qt || 1) - 1 } : { ...p }
+      );
+      setcart(updatedCart);
     }
   }
+  console.log({ cart });
 
   return (
     <StyledWrapper>
       Kit page
       <br /> <br />
       {products.map((p) => {
-        // const product = products.filter((prod) => prod.pId === p.pid);
         return (
           <div
             style={{
@@ -59,10 +64,9 @@ export const Kit = () => {
               padding: "2rem",
             }}
           >
-            Product Name: {p.pName} Quantity: {}
-            //localstorage.key(product[])
-            <button onClick={() => add(p.pId)}>+</button>
-            <button onClick={() => remove(p.pId)}>-</button>
+            Product Name: {p.pName}
+            <button onClick={() => add(p)}>+</button>
+            <button onClick={() => remove(p)}>-</button>
           </div>
         );
       })}
