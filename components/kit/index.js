22c749381612f22e11af000a12d088560141017c
batrasudhanshu <batrasudhanshu09@gmail.com>
2021-07-12 01:06:58 +0530

    Kit Page

--- components/kit/index.js
+++ components/kit/index.js
@@ -1,59 +1,110 @@
-import { useEffect, useState } from "react";
-import { IsCSR } from "../../util/common";
+// import { useEffect, useState } from "react";
+// import { IsCSR } from "../../util/common";
+import Link from "next/link";
+import { FaIcon } from "../BaseComponent/FaIcon";
 import StyledWrapper from "./style.kit";
-const allProd = [
-  { pId: "product1", pName: "kit 1" },
-  { pId: "product2", pName: "kit 2" },
+// const allProd = [
+//   { pId: "product1", pName: "kit 1" },
+//   { pId: "product2", pName: "kit 2" },
+// ];
+const kitItems = [
+  {
+    title: "Razor",
+    image: "/kitAssets/razor.png",
+    content:
+      "Get the job done with this four-blade razor, featuring a textured metal handle for an unrivaled grip and effortless glide. Comes with additional blades.",
+  },
+  {
+    title: "Razor case",
+    image: "/kitAssets/razor_cover.png",
+    content:
+      "Your safety is our top priority, but that doesn’t mean you have to sacrifice convenience. This razor case is easy-to-use and free from fiddly clips and locks.",
+  },
+  {
+    title: "Bamboo toothbrush",
+    image: "/kitAssets/toothbrush.png",
+    content:
+      "Smooth to the touch, our bamboo toothbrush features medium-grade, wave-shaped bristles for an optimal clean. It’s also 100 per cent biodegradable and environmentally-friendly.",
+  },
+  {
+    title: "Dupont Tyvek bag",
+    image: "/kitAssets/bag.png",
+    content:
+      "Durable, waterproof, and stylish, this Dupont Tyvek bag keeps your toiletries securely in their place during transit.",
+  },
+  {
+    title: "Soap",
+    image: "/kitAssets/soap.png",
+    content:
+      "Australian-made, this high-quality soap will keep you smelling and feeling fresh and squeaky clean, even when on the go.",
+  },
+  {
+    title: "Dental floss",
+    image: "/kitAssets/floss.png",
+    content:
+      "Made from cornstarch, this dental floss is stored in a durable yet flexible glass jar sturdy enough to stand up to long-haul travel.",
+  },
+  {
+    title: "Shaving soap",
+    image: "/kitAssets/soap.png",
+    content:
+      "With a thick, rich lather and invigorating citrus scent, our shaving soap and tin ensures you get a clean, comfortable shave when and wherever you need.",
+  },
+  {
+    title: "Soap case",
+    image: "/kitAssets/soap_case.png",
+    content:
+      "Travel cases are a must for any jet setter. Our leak-free case ensures your soap stays fresh and clean.",
+  },
 ];
-
 export const Kit = () => {
   // const cartList = JSON.parse(localStorage.getItem("cart"));
   // console.log({ cartList });
 
-  const [cart, setcart] = useState([]);
-  const [products, setproducts] = useState(allProd);
+  // const [cart, setcart] = useState([]);
+  // const [products, setproducts] = useState(allProd);
 
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("cart"));
-    if (storedData) {
-      setcart(storedData);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const storedData = JSON.parse(localStorage.getItem("cart"));
+  //   if (storedData) {
+  //     setcart(storedData);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart]);
+  // useEffect(() => {
+  //   localStorage.setItem("cart", JSON.stringify(cart));
+  // }, [cart]);
 
-  function add(product) {
-    const prod = cart.find((p) => p.pId === product.pId);
-    if (!prod) {
-      setcart((oldCart) => [...oldCart, { ...product, qt: 1 }]);
-    } else {
-      const updatedCart = cart.map((p) =>
-        p.pId === prod.pId ? { ...p, qt: p.qt + 1 } : { ...p }
-      );
-      setcart(updatedCart);
-    }
-  }
-  function remove(product) {
-    const prod = cart.find((p) => p.pId === product.pId);
-    if (!prod) {
-      //skip and do nothing
-    } else if (prod.qt === 1) {
-      const updatedCart = cart.filter((f) => f.pId !== prod.pId);
-      setcart(updatedCart);
-    } else {
-      const updatedCart = cart.map((p) =>
-        p.pId === prod.pId ? { ...p, qt: (p.qt || 1) - 1 } : { ...p }
-      );
-      setcart(updatedCart);
-    }
-  }
-  console.log({ cart });
+  // function add(product) {
+  //   const prod = cart.find((p) => p.pId === product.pId);
+  //   if (!prod) {
+  //     setcart((oldCart) => [...oldCart, { ...product, qt: 1 }]);
+  //   } else {
+  //     const updatedCart = cart.map((p) =>
+  //       p.pId === prod.pId ? { ...p, qt: p.qt + 1 } : { ...p }
+  //     );
+  //     setcart(updatedCart);
+  //   }
+  // }
+  // function remove(product) {
+  //   const prod = cart.find((p) => p.pId === product.pId);
+  //   if (!prod) {
+  //skip and do nothing
+  //   } else if (prod.qt === 1) {
+  //     const updatedCart = cart.filter((f) => f.pId !== prod.pId);
+  //     setcart(updatedCart);
+  //   } else {
+  //     const updatedCart = cart.map((p) =>
+  //       p.pId === prod.pId ? { ...p, qt: (p.qt || 1) - 1 } : { ...p }
+  //     );
+  //     setcart(updatedCart);
+  //   }
+  // }
+  // console.log({ cart });
 
   return (
     <StyledWrapper>
-      Kit page
+      {/* Kit page
       <br /> <br />
       {products.map((p) => {
         return (
@@ -69,7 +120,90 @@ export const Kit = () => {
             <button onClick={() => remove(p)}>-</button>
           </div>
         );
-      })}
+      })} */}
+      <div className="kit">
+        <div className="kit__top">
+          <div className="kit__top-left">
+            <div className="kit__top-cover">
+              <img src="/kitAssets/cover.png" />
+            </div>
+          </div>
+          <div className="kit__top-right">
+            <h3>MYGIENE GROOMING KIT</h3>
+            <h2 className="kit-price">USD $59.99</h2>
+            <h2 className="kit-quantity">Quantity</h2>
+            <div className="quantity-buttons">
+              <button>
+                <FaIcon className="fa fa-minus button-icon" />
+              </button>
+              <span>1</span>
+              <button>
+                <FaIcon className="fa fa-plus button-icon" />
+              </button>
+            </div>
+            <div className="cart-button">
+              <button>
+                <span>Add to Cart</span>
+              </button>
+            </div>
+            <div className="values">
+              <div className="value-1">
+                <span>ECO-FRIENDLY</span>
+                <br />
+                <img src="/homeAssets/eco-friendly.png" />
+              </div>
+              <div className="value-2">
+                <span>SUSTAINABLE</span>
+                <br />
+                <img src="/homeAssets/sustainable.png" />
+              </div>
+              <div className="value-3">
+                <span>HIGH QUALITY</span>
+                <br />
+                <img src="/homeAssets/high-quality.png" />
+              </div>
+            </div>
+            <div className="description">
+              <details open>
+                <summary>Product Description</summary>
+                <p>
+                  Mygiene delivers a complete, one-stop toiletries package at
+                  the click of a button. You get more time to kick back, relax,
+                  and enjoy your holiday.
+                </p>
+              </details>
+            </div>
+            <div className="shipping">
+              <Link href="/refund">
+                <a href="/refund" target="blank">
+                  <span>Shipping & Returns</span>
+
+                  <FaIcon className="fa fa-external-link" />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="kit__tiles">
+          <div className="kit__tiles-title">
+            <span>Kit Items</span>
+          </div>
+          <div className="kit__tiles-items">
+            {kitItems.map((m) => (
+              <div class="grid-item">
+                <span>
+                  {m.title} <FaIcon className="fa fa-arrow-down" />
+                </span>
+
+                <img src={m.image} />
+                <div className="text-overlay">
+                  <span>{m.content}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </StyledWrapper>
   );
 };
--- components/kit/style.kit.js
+++ components/kit/style.kit.js
@@ -1,5 +1,317 @@
 import styled from "styled-components";
 
 export default styled.div`
-  margin-top: 5rem;
+  margin-top: 4.5rem;
+
+  .kit {
+    margin: auto;
+    width: 90%;
+    /* position: relative; */
+    margin-top: 8rem;
+    margin-bottom: 2rem;
+
+    .kit__top {
+      display: flex;
+      grid-gap: 5%;
+
+      .kit__top-left {
+        width: 100%;
+        .kit__top-cover {
+          background-color: #ffeeee;
+          border-radius: 6px;
+          img {
+            background-position: center;
+            object-fit: cover;
+            width: 100%;
+          }
+        }
+      }
+      .kit__top-right {
+        width: 90%;
+        h3 {
+          font-weight: 700;
+          font-size: 1.65rem;
+          margin-bottom: 1rem;
+          margin-top: 1rem;
+          line-height: 2rem;
+        }
+        .kit-price {
+          font-weight: 650;
+          font-size: 1.55rem;
+          margin: 0;
+        }
+        .kit-quantity {
+          font-weight: 650;
+          font-size: 1.6rem;
+
+          margin-top: 2rem;
+          color: #8c8c8c;
+          margin-bottom: 0.7rem;
+        }
+        .quantity-buttons {
+          display: flex;
+          grid-gap: 1rem;
+
+          button {
+            display: flex;
+            width: 35px;
+            height: 35px;
+            justify-content: center;
+            align-items: center;
+            border: 2px solid #bdbdbd;
+            .button-icon {
+              font-weight: 700;
+              font-size: 1rem;
+              color: darkgray;
+            }
+          }
+          span {
+            width: 31px;
+            height: 31px;
+            border: 2px solid #bdbdbd;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: darkgray;
+            font-weight: 700;
+            font-size: 1.3rem;
+          }
+        }
+        .cart-button {
+          margin-top: 2.5rem;
+          button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 174px;
+            height: 45px;
+            background: #f2f2f2;
+            border-radius: 8px;
+            border: none;
+
+            span {
+              font-weight: 750;
+              font-size: 1.05rem;
+              text-transform: uppercase;
+            }
+          }
+        }
+        .values {
+          display: flex;
+          grid-gap: 1rem;
+          margin-top: 2rem;
+          .value-1,
+          .value-2,
+          .value-3 {
+            text-align: center;
+          }
+          span {
+            color: #d8d8d8;
+            font-size: 0.9rem;
+          }
+          img {
+            /* padding-left: 2rem; */
+            padding-top: 0.5rem;
+            width: 45px;
+            height: 45px;
+          }
+        }
+        .description {
+          margin-top: 2rem;
+          details {
+            summary {
+              margin: 1rem 0rem;
+              list-style-type: none;
+              text-transform: uppercase;
+              font-size: 1.1rem;
+              font-weight: 600;
+            }
+            summary::marker {
+              display: none;
+            }
+            summary:after {
+              content: "+";
+              color: #fff;
+              float: left;
+              font-size: 1.8rem;
+              font-weight: 400;
+              margin: -7px 10px 0 0;
+              padding: 0px 3px;
+              color: black;
+              text-align: center;
+              width: 20px;
+              border: 1px solid black;
+            }
+            p {
+              width: 80%;
+              line-height: 2rem;
+
+              margin-left: 39px;
+              color: gray;
+            }
+          }
+          details[open] summary:after {
+            content: "-";
+            padding: 0px 3px;
+            margin: -7px 10px 0 0;
+          }
+        }
+        .shipping {
+          margin-top: 2rem;
+          a {
+            display: flex;
+
+            span {
+              font-size: 1.2rem;
+              font-weight: 600;
+            }
+            i {
+              padding-left: 0.5rem;
+            }
+          }
+        }
+      }
+    }
+    .kit__tiles {
+      margin-top: 2rem;
+      .kit__tiles-title {
+        width: 174px;
+        height: 45px;
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        span {
+          color: white;
+          font-size: 1.05rem;
+          text-transform: uppercase;
+        }
+      }
+      .kit__tiles-items {
+        width: 75%;
+        margin: auto;
+        margin-top: 2rem;
+        display: grid;
+        grid-gap: 3rem;
+        justify-content: center;
+        grid-template-columns: 25% 25% 25% 25%;
+        .grid-item {
+          background: #fff9f9;
+          border-radius: 8px;
+          padding: 20px;
+          font-size: 30px;
+          text-align: center;
+          height: 200px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          position: relative;
+          .text-overlay {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.8);
+            display: none;
+            justify-content: center;
+            align-items: center;
+            margin: auto;
+            span {
+              color: white;
+              height: 100%;
+              line-height: 1.45rem;
+              left: 0;
+              top: 0;
+              width: 90%;
+              /* padding: 5%; */
+              margin: 7%;
+            }
+          }
+          span {
+            top: 1rem;
+            left: 1rem;
+            position: absolute;
+            font-size: 1.05rem;
+            font-weight: 500;
+            text-transform: capitalize;
+          }
+
+          img {
+            object-fit: cover;
+            background-position: center;
+          }
+        }
+        .grid-item:hover {
+          .text-overlay {
+            position: absolute;
+
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.8);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.5s ease-in-out;
+
+            margin: auto;
+            span {
+              color: white;
+              height: 100%;
+              line-height: 1.45rem;
+              left: 0;
+              top: 0;
+              width: 90%;
+
+              /* padding: 5%; */
+              margin: 7%;
+            }
+          }
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 425px) {
+    .kit {
+      .kit__top {
+        display: block !important;
+        .kit__top-right {
+          width: 100% !important;
+          margin-top: 3rem !important;
+          h3 {
+            font-size: 2rem !important;
+          }
+          .kit-price {
+            font-weight: 600 !important;
+            font-size: 1.8rem !important;
+          }
+          .description {
+            details {
+              p {
+                width: 100% !important;
+                font-size: 1.25rem !important;
+              }
+            }
+          }
+          .cart-button {
+            button {
+              span {
+                font-size: 1.3rem !important;
+              }
+            }
+          }
+        }
+      }
+      .kit__tiles {
+        display: grid;
+        .kit__tiles-items {
+          width: 90%;
+          grid-template-columns: 50% 50%;
+          .grid-item {
+            span {
+              font-size: 1.5rem;
+            }
+          }
+        }
+      }
+    }
+  }
 `;
