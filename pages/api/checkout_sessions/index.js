b40b6074b218ed88510dcba6f7a2c87f44234d73
Rakshit Maini <rakshit.maini@bivocalbirds.com>
2021-07-25 04:08:04 +0530

    minor fix

--- pages/api/checkout_sessions/index.js
+++ pages/api/checkout_sessions/index.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         mode: "payment",
         payment_method_types: ["card"],
         line_items: req?.body?.items ?? [],
-        success_url: `${URL}/home?session_id={CHECKOUT_SESSION_ID}`,
+        success_url: `${URL}?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${URL}/cart`,
       });
 
