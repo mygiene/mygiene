a7f0d892d550dd36a34a530cba8628562d8b3bae
batrasudhanshu <batrasudhanshu09@gmail.com>
2021-07-27 23:10:29 +0530

    minor Fix again

--- pages/admin/user-list/[user].jsx
+++ pages/admin/user-list/[user].jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { firestore } from "../../../firebase/utils";
-import User from "../../../components/UsersList/user";
+import User from "../../../components/UsersList/User";
 
 //const u =await  firestore.doc(`users/${Router.pathname}`).get(); u.data();
 const user = () => {
