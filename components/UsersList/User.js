3915c42c8d3fca399c18420bfef85e659a6d3c39
batrasudhanshu <batrasudhanshu09@gmail.com>
2021-08-05 23:38:03 +0530

    User Page (Admin Panel)

--- components/UsersList/User.js
+++ components/UsersList/User.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { firestore } from "../../firebase/utils";
-
+import UserItemWrapper from "./style.useritem";
+import OrderItem from "../OrdersPage/OrderItem";
+import Link from "next/link";
 //const u =await  firestore.doc(`users/${Router.pathname}`).get(); u.data();
 const User = () => {
   const Router = useRouter();
-  const [userDetails, setUserDetails] = useState();
+  const [userDetails, setUserDetails] = useState([]);
   const [orders, setOrders] = useState();
   const fetchInfo = async () => {
     const userId = Router.query.user;
     const user = await firestore.doc(`users/${userId}`).get();
-    setUserDetails(user.data());
+    setUserDetails({ ...user.data(), id: user.id });
 
     const allOrders = [];
     const response = firestore
@@ -26,8 +28,86 @@ const User = () => {
     if (Router.query.user) fetchInfo();
   }, []);
   console.log("orders", orders);
-  console.log(userDetails);
-  return <div>users page</div>;
+  console.log("user", userDetails);
+
+  return (
+    <UserItemWrapper>
+      <div className="user-item">
+        <div className="heading">
+          <h2>User Page # {userDetails.id}</h2>
+        </div>
+        <div className="content">
+          <div className="profile_details">
+            <div className="profile_details-name">
+              <span>Name </span>
+              <span>{userDetails?.displayName}</span>
+            </div>
+            <div className="profile_details-email">
+              <span>Email Id</span>
+              <span>{userDetails?.email}</span>
+            </div>
+            <div className="profile_details-contact">
+              <span>Contact No</span>
+              <div>
+                <span>{userDetails?.mobile || "No Record"}</span>
+              </div>
+            </div>
+
+            {/* <div className="profile_details-address">
+              <span>Address</span>
+
+              <div>
+                {userDetails?.address?.length > 0 &&
+                  userDetails?.address.map((m) => (
+                    <div>
+                      <span>{m.address}</span>
+                      <span>({m.type})</span>
+                    </div>
+                  ))}
+              </div>
+            </div> */}
+          </div>
+
+          <div className="order-details">
+            {orders?.length > 0 &&
+              orders.map((item, index) => (
+                <Link
+                  href={`http://localhost:3010/admin/orders-list/${item.id}`}
+                >
+                  <a>
+                    <div className="order-item">
+                      <div className="order__card">
+                        <div className="order__card-image">
+                          <img src="/kitAssets/cover.png" />
+                        </div>
+                        <div className="order__card-content">
+                          <div className="content-outer">
+                            <h3>mygiene grooming kit</h3>
+                            <div className="quantity-order">
+                              <span className="order-outer">
+                                <strong>Order Id :</strong>{" "}
+                                <span> {item.id}</span>
+                              </span>
+                            </div>
+                            <div className="price">
+                              <span>
+                                <strong>Quantity : </strong>
+                                {item.cartItems.qt}
+                              </span>{" "}
+                              <strong>Total : ${item.totalAmount}</strong>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              ))}
+          </div>
+        </div>
+      </div>
+    </UserItemWrapper>
+  );
 };
 
 export default User;
---
+++ components/UsersList/style.useritem.js
@@ -0,0 +1,216 @@
+import styled from "styled-components";
+import variables from "../../styles/variables";
+export default styled.div`
+  .user-item {
+    margin-top: 7rem;
+    padding: 0;
+    width: 100%;
+    margin-bottom: 3rem;
+
+    .heading {
+      display: flex;
+      width: 90%;
+      margin: auto;
+      justify-content: center;
+      h2 {
+        font-size: 1.8rem;
+        font-weight: 700;
+        font-style: italic;
+      }
+    }
+    .content {
+      display: flex;
+      width: 90%;
+      min-height: 300px;
+      margin: auto;
+      justify-content: space-between;
+      .profile_details {
+        display: flex;
+        flex-direction: column;
+        grid-gap: 1.5rem;
+        width: 35%;
+        margin-top: 1.5rem;
+        .profile_details-name,
+        .profile_details-email,
+        .profile_details-contact,
+        .profile_details-address {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        .profile_details-address {
+          div {
+            display: flex;
+            flex-direction: column;
+            grid-gap: 0.8rem;
+
+            div {
+              display: flex;
+              grid-gap: 1rem;
+              flex-direction: row;
+            }
+          }
+        }
+      }
+    }
+    hr {
+      display: none;
+    }
+    .order-details {
+      a {
+        color: black;
+      }
+      .order-item {
+        display: flex;
+        margin-top: 1rem;
+        flex-direction: column;
+        justify-content: flex-start;
+        margin-bottom: 3rem;
+        .order-status {
+          display: block;
+          grid-gap: 41%;
+          margin-bottom: 0.3rem;
+          grid-gap: 0.4rem;
+          .first-step {
+            display: flex;
+            float: left;
+            flex-direction: column;
+            align-items: flex-start;
+            span:nth-child(1) {
+              color: #004525;
+              font-size: 1.1rem;
+              font-weight: 700;
+            }
+            span:nth-child(2) {
+              color: #004525;
+              font-size: 1rem;
+              font-weight: 400;
+            }
+          }
+          .current-step {
+            float: right;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            span:nth-child(1) {
+              color: #004525;
+              font-size: 1.1rem;
+              font-weight: 700;
+            }
+            span:nth-child(2) {
+              color: #004525;
+              font-size: 1rem;
+              font-weight: 400;
+            }
+          }
+        }
+        .order__card {
+          width: 400px;
+          height: max-content;
+          background-color: #ebebeb;
+          align-items: center;
+          display: flex;
+          grid-gap: 1rem;
+          position: relative;
+          flex-direction: column;
+          border-radius: 8px;
+          .order__card-image {
+            width: 35%;
+            margin-top: 1.1rem;
+            background-color: ${variables.primary_color};
+
+            border-radius: 50%;
+
+            img {
+              background-position: center;
+              object-fit: cover;
+              width: 100%;
+              height: 100%;
+            }
+          }
+          .order__card-content {
+            display: flex;
+            flex-direction: column;
+            grid-gap: 2rem;
+            width: 90%;
+            text-align: center;
+            margin-bottom: 2rem;
+
+            .content-outer {
+              display: flex;
+              flex-direction: column;
+              h3 {
+                text-transform: uppercase;
+                font-size: 1rem;
+                margin-bottom: 0;
+              }
+              .price {
+                margin-top: 1.3rem;
+                font-weight: 900;
+                display: flex;
+                justify-content: space-between;
+              }
+              .quantity-order {
+                display: flex;
+                margin-top: 1.5rem;
+                grid-gap: 0.7rem;
+                justify-content: space-between;
+
+                .order-outer {
+                  display: flex;
+                  align-items: baseline;
+                  span {
+                    font-size: 1.05rem;
+                    color: darkslategray;
+                    font-weight: 700;
+                    padding-left: 0.25rem;
+                  }
+                }
+              }
+            }
+            div:nth-child(2) {
+              button {
+                color: red;
+                font-weight: 600;
+                padding-left: 0;
+                font-size: 0.8rem;
+                border: none;
+                text-decoration: underline;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 767px) {
+    .user-item {
+      .heading {
+        h2 {
+          font-size: 1.35rem;
+          line-height: 1.8rem;
+        }
+      }
+      .content {
+        flex-direction: column;
+        .profile_details {
+          width: 100%;
+        }
+
+        .order-details {
+          margin-top: 2rem;
+          a {
+            .order-item {
+              .order__card {
+                width: 100%;
+                .order__card-image {
+                  width: 40%;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
