2afaac8c03812cc1c3ce93c65e68ecd1df54d613
batrasudhanshu <batrasudhanshu09@gmail.com>
2021-08-05 16:59:52 +0530

    FAQs Page

--- components/FaqsPage/index.js
+++ components/FaqsPage/index.js
@@ -1,10 +1,64 @@
 import React from "react";
 import Banner from "../BaseComponent/Banner";
 import FaqsWrapper from "./style.faqs";
+
+const faqsContent = [
+  {
+    ques: "What makes the product eco-friendly?",
+    ans: "Mygiene incorporates a number of different recyclable materials to support a healthy impact on the environment. It’s never been easier to swap plastic products and materials for eco-friendly alternatives.",
+  },
+  {
+    ques: "How long does the kit last?",
+    ans: "We’ve designed each product in the kit to last for 30 days. This means the kit is ideal for use across the full duration of the average holiday. You can use it every day if required, or you can simply call upon the kit as and when you require. Order as often as you like so you always have what you need.",
+  },
+  {
+    ques: "Are the items advertised available for individual purchase?",
+    ans: "No, we provide Mygiene as an all-inclusive package.",
+  },
+  {
+    ques: "Do you offer refills for the kit?",
+    ans: "We don’t provide refills, unfortunately, but your razor handle is compatible with the Gillette Mach 3 razor attachment available from your closest supermarket. Also, your soap case is designed to fit a standard 100ml bar of soap.",
+  },
+  {
+    ques: "Can I purchase Mygiene at the supermarket?",
+    ans: "Our kits aren’t available in stores at the present time.",
+  },
+  {
+    ques: "Are travel kits worthwhile?",
+    ans: "Travel kits give customers plenty of value, thanks to their included bags and organizational elements. Many of our kits are designed with special storing features and functions to help you get even more from the product.",
+  },
+  {
+    ques: "Wait, I still want to know more?",
+    ans: "No worries! You can email us at info@mygiene.com.au, or message us on Instagram or Facebook @mygiene_aus.",
+  },
+];
 const FaqsPage = () => {
   return (
     <FaqsWrapper>
       <Banner title="Frequently Asked Questions (FAQs)" Image="/banner_1.jpg" />
+
+      <div className="faqs">
+        <div className="heading">
+          <h3>We're are here to answer all your questions</h3>
+        </div>
+        <div className="content">
+          {faqsContent.map((item, index) =>
+            index === 0 ? (
+              <details open>
+                <summary>
+                  <span>{item.ques}</span>
+                </summary>
+                <p>{item.ans}</p>
+              </details>
+            ) : (
+              <details>
+                <summary>{item.ques}</summary>
+                <p>{item.ans}</p>
+              </details>
+            )
+          )}
+        </div>
+      </div>
     </FaqsWrapper>
   );
 };
--- components/FaqsPage/style.faqs.js
+++ components/FaqsPage/style.faqs.js
@@ -1,2 +1,153 @@
 import styled from "styled-components";
-export default styled.div``;
+export default styled.div`
+  .faqs {
+    width: 90%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+
+    .heading {
+      text-align: center;
+      h3 {
+        text-transform: uppercase;
+        font-size: 1.5rem;
+        color: darkslategray;
+      }
+    }
+    .content {
+      details {
+        margin-bottom: 2rem;
+        summary {
+          margin: 1rem 0rem 1.5rem 0rem;
+          list-style-type: none;
+          text-transform: uppercase;
+          font-size: 1.3rem;
+          font-weight: 600;
+        }
+        summary::marker {
+          display: none;
+        }
+        summary:after {
+          content: "+";
+          color: #fff;
+          float: left;
+          font-size: 1.8rem;
+          font-weight: 400;
+          margin: -7px 10px 0 0;
+          padding: 0px 3px;
+          color: black;
+          text-align: center;
+          width: 20px;
+          border: 1px solid black;
+        }
+      }
+      details[open] summary:after {
+        content: "-";
+        padding: 0px 3px;
+        margin: -7px 10px 0 0;
+      }
+      p {
+        padding-left: 40px;
+        line-height: 1.5rem;
+        font-size: 1.1rem;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 768px) and (min-width: 640px) {
+    .faqs {
+      width: 93%;
+
+      .content {
+        details {
+          summary {
+            font-size: 1.13rem;
+          }
+          p {
+            font-size: 1.1rem;
+          }
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 639px) and (min-width: 426px) {
+    .faqs {
+      width: 93%;
+
+      .content {
+        details {
+          summary {
+            font-size: 1.13rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            gap: 1rem;
+          }
+          summary::after {
+            content: "+";
+            color: #fff;
+            float: left;
+            font-size: 1.5rem;
+            font-weight: 400;
+            margin: -7px 10px 0 0;
+            padding: 0px 3px;
+            color: black;
+            text-align: center;
+            width: 13px;
+            border: 1px solid black;
+          }
+          p {
+            font-size: 1.1rem;
+            padding-left: 0px;
+            padding-right: 40px;
+          }
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 425px) {
+    .faqs {
+      width: 93%;
+
+      .content {
+        details {
+          margin-bottom: 1.5rem;
+          summary {
+            font-size: 1rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            gap: 1rem;
+          }
+          summary::after {
+            content: "+";
+            color: #fff;
+            float: left;
+            font-size: 1.3rem;
+            font-weight: 400;
+            margin: -7px 5px 0 0;
+            padding: -1px 3px;
+            color: black;
+            text-align: center;
+            width: 9px;
+            border: 1px solid black;
+          }
+          p {
+            font-size: 1.1rem;
+            padding-left: 0px;
+            padding-right: 30px;
+          }
+        }
+        details[open] summary:after {
+          content: "-";
+          padding: 1px 3px;
+          margin: auto;
+          margin: -7px 5px 0 0;
+          width: 13px;
+        }
+      }
+    }
+  }
+`;
