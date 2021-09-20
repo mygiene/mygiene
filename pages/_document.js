91247b5e60cd031e465301f7462278f4a34d9505
batrasudhanshu <batrasudhanshu09@gmail.com>
2021-09-21 00:19:19 +0530

    Favicon update

--- pages/_document.js
+++ pages/_document.js
@@ -24,10 +24,16 @@ export default class MyDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
+          <link
+            rel="mask-icon"
+            tyoe="image/png"
+            href="/safari-pinned-tab.svg"
+            color="#ff0000"
+          />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="theme-color" content="#f5f5f5" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
             rel="stylesheet"
